ca46a1d99a7d9ee916eded50b75c2aeb7befc523
NoahFreelove <90488068+NoahFreelove@users.noreply.github.com>
2023-01-26 00:52:49 -0500

    renamed var

--- src/main/java/JE/Utility/Delayer.java
+++ src/main/java/JE/Utility/Delayer.java
@@ -4,8 +4,8 @@ public class Delayer {
     public boolean activated = false;
     private long duration;
     private long startTimestamp;
-    public Delayer(long durationSeconds){
-        this.duration = durationSeconds;
+    public Delayer(long durationInMilliseconds){
+        this.duration = durationInMilliseconds;
         this.startTimestamp = System.currentTimeMillis();
     }
     public boolean check(){
