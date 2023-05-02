c883656d71eb86bc204642e95d5846dc8f2f53ae
Noah Freelove <90488068+NoahFreelove@users.noreply.github.com>
2023-05-02 11:08:16 -0400

    Made TriggerEvent default methods

--- src/main/java/org/JE/JE2/Objects/Scripts/Physics/TriggerEvent.java
+++ src/main/java/org/JE/JE2/Objects/Scripts/Physics/TriggerEvent.java
@@ -3,7 +3,7 @@ package org.JE.JE2.Objects.Scripts.Physics;
 import org.JE.JE2.Objects.GameObject;
 
 public interface TriggerEvent{
-    void onTriggerEnter(GameObject other);
-    void onTriggerExit(GameObject other);
-    void onTrigger(GameObject other);
+    default void onTriggerEnter(GameObject other){}
+    default void onTriggerExit(GameObject other){}
+    default void onTrigger(GameObject other){}
 }
